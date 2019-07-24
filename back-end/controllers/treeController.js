20a42772408a81141da91c3cdbd150692c28565c
luz fernandez <luz.fernandez01@uptc.edu.co>
2019-07-23 22:02:49 -0500

    Revert "desarrollo de lowestCommonAncestor"
    This reverts commit b57a48fd81e5bd3e430911400694acaace0a95b7.

--- back-end/controllers/treeController.js
+++ back-end/controllers/treeController.js
@@ -1,6 +1,5 @@
 const Tree = require('../DAO/binaryTreeDao');
 let treeData = [];
-
 //creacion del arbol
 exports.createBinaryTree = (req, res, next) => {
     console.log(req.body);
@@ -19,24 +18,21 @@ exports.createBinaryTree = (req, res, next) => {
     });
 };
 
-
-//****** manejo menor ancestro comun******//
-
-//llamado de informacion del arbol desde db
-loadTreeData = (id, res, callback) => {
+//menor ancestro comun
+documents = (id, callback) => {
     Tree.find({id: id}, function (err, result) {
         if (err) {
             callback(err, null);
         } else {
-            result = {'lca': callback(null, result)};
-            res.send(result);
+            callback(null, result);
         }
     });
 };
 
-//retorna lista de ascendientes segun el nodo
-getAncestors = (idNode, fathers) => {
+//retorna lista de padres segun el nodo
+getFather = (idNode, fathers) => {
     let childNode = {};
+
     for (let i = 0; i < nodes.length; i++) {
         if (nodes[i].id == idNode) {
             childNode = nodes[i];
@@ -45,47 +41,44 @@ getAncestors = (idNode, fathers) => {
     }
 
     if (childNode.father != -1) {
-        getAncestors(childNode.father, fathers)
+        getFather(childNode.father, fathers)
     }
     return fathers;
 };
 
-//reordena el grafo y busca los ascendientes del nodo
-ordenedNodeAncestors = (idNode) => {
+//ancestros del nodo
+nodeAncestors = (idNode) => {
     nodes = treeData.nodes.reverse();
     let fathers = [];
-    getAncestors(idNode, fathers);
+    getFather(idNode, fathers);
+
+    console.log("ancestros ", fathers);
     return fathers;
 };
 
-//establece el ascendente mas cercano
-firstCommonAncestor = (ancestors1, ancestors2) => {
-    let result;
-    for (let an1 of ancestors1) {
-        for (let an2 of ancestors2) {
-            if (an1 === an2 && an1 !== -1) {
-                result = an1;
-                return an1;
+commonAncestor = (ancestors1, ancestors2) => {
+    ancestors1.forEach((a1) => {
+        ancestors2.forEach((a2) => {
+            if (a1 === a2) {
+                return a1;
             }
-        }
-    }
-    return result;
+        })
+    })
 };
 
 exports.lowestCommonAncestor = async (req, res, next) => {
-
-    let nana = [];
-    let d = await loadTreeData(req.params.id, res, (err, doc) => {
+    let d1 = await documents(req.params.id, function (err, doc, res) {
         if (err) {
             console.log(err);
-            return err;
+            res.send(err);
         }
         treeData = doc[0];
-        n1a = ordenedNodeAncestors(req.body.node1);
-        n2a = ordenedNodeAncestors(req.body.node2);
-        return firstCommonAncestor(n1a, n2a);
+        n1a = nodeAncestors(req.body.node1);
+        n2a = nodeAncestors(req.body.node2);
+        console.log(commonAncestor(n1a, n2a), " anterior comun");
     });
 
+    res.send("response e.e")
 };
 
 
@@ -118,20 +111,46 @@ exports.getTrees = async (req, res) => {
 //editar arbol
 exports.editTree = async (req, res) => {
     const id = parseInt(req.params.id);
-    const newTree = {
+    const newUser = {
+        tipo_id: req.body.tipo_id,
         id: req.body.id,
-        nodes: req.body.nodes,
-        edges: req.body.edges
+        nombre: req.body.nombre,
+        apellido: req.body.apellido,
+        fecha_de_nacimiento: req.body.fecha_nacimiento,
+        sexo: req.body.sexo,
+        celular: req.body.celular,
+        correo: req.body.correo,
+        nivel_Salarial: req.body.nivel_Salarial,
+        historia_Clinica: req.body.historia_Clinica,
+        afiliado_activo: req.body.afilidiado,
+        benecificiarios: req.body.benecificiarios,
+        plan_Complementario: req.body.plan_Complementario,
+        citas_medicas: req.body.citas_medicas,
+        tipo_afiliado: req.body.tipo_afiliado,
     };
+    await Tree.findOneAndUpdate({id: id}, {$set: newUser}, {new: true}).then(
+        console.log("se supone que se actualizo")
+    );
 
-    await Tree.findOneAndUpdate({id: id}, {$set: newTree}, {new: true})
 
     Tree.findOne({id: id}, (err, doc) => {
         if (err) {
-            res.status(200).send("Server Error - Something wrong when find tree!");
+            res.status(200).send("Server Error - Something wrong when find user!");
         }
-
-        res.send(doc)
+        const expiresIn = 24 * 60 * 60;
+        const accessToken = jwt.sign({id: doc._id}, sec_key,
+            {
+                expiresIn: expiresIn
+            });
+        const data = {
+            id: doc.id,
+            name: doc.nombre,
+            expire: expiresIn,
+            accessToken: accessToken,
+            usuario: doc
+        };
+        //manda a front
+        res.send({data})
     });
 };
 
